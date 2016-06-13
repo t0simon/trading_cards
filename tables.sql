74023796701ea4e3a74aaa26212905e566fb6050
t0simon <simon.toermer@gmx.de>
2016-06-13 16:40:39 +0200

    Update tables.sql

--- tables.sql
+++ tables.sql
@@ -43,7 +43,7 @@ INSERT INTO `Items` (`id`, `type`, `CustomName`, `Size`, `Age`) VALUES
 (2, 'Elefant', 'Hans', 350, 31),
 (8, 'Katze', 'Hans', 20, 3),
 (9, 'Katze', 'Katze', 20, 3),
-(28, 'Elefant', 'HÃ¤ssliches RÃ¼sseltier', 350, 31),
+(28, 'Elefant', 'Elefant', 350, 31),
 (29, 'Elefant', 'Drumbo', 350, 31),
 (48, 'Giraffe', 'Adelheit', 310, 4),
 (49, 'Giraffe', 'Giraffie', 310, 4),
@@ -84,7 +84,7 @@ INSERT INTO `Types` (`type`, `picture`, `name`, `latName`, `CustomNameDefault`, 
 ('Giraffe', 'https://upload.wikimedia.org/wikipedia/commons/6/68/Giraffen.jpg', 'Giraffe', 'Giraffa camelopardalis', 'Giraffie', 'Die Giraffe ist ein Säugetier aus der Ordnung der Paarhufer und die einzige Art der Gattung Giraffa. Sie ist das höchste landlebende Tier der Welt.', 310, 4),
 ('Katze', 'http://localhost/SRZ/wallpaper.png', 'Hauskatze', 'Felis silvestris catus', 'K&auml;tzchen', 'Die Hauskatze ist eine Unterart der Wildkatze und deren Haustierform. Sie ist ein Fleischfresser und zählt zu den beliebtesten Heimtieren.', 20, 3),
 ('Loewe', 'https://upload.wikimedia.org/wikipedia/commons/e/e0/Panthera_leo_bleyenberghi_2013.jpg', 'L&ouml;we', 'Panthera leo', 'Leo', 'Der Löwe ist eine Art der Katzen. Er lebt im Unterschied zu anderen Katzen in Rudeln, ist durch die Mähne des Männchens gekennzeichnet und ist heute in Afrika sowie im indischen Bundesstaat Gujarat beheimatet.', 190, 10),
-('Rabe', 'http://www.herr-m.at/wp-content/uploads/2011/04/Rabensteinbruch-92BA.jpg', 'Kolkrabe', 'Corvus corax', 'Korax', 'Ein schÃ¶ner schwarzer Vogel mit melodischem Ruf im Gegensatz zum KrÃ¤chzen der KrÃ¤he.', 30, 7);
+('Rabe', 'https://upload.wikimedia.org/wikipedia/commons/0/01/Common_Raven_in_Jersey.jpg', 'Kolkrabe', 'Corvus corax', 'Korax', 'Ein schÃ¶ner schwarzer Vogel mit melodischem Ruf im Gegensatz zum KrÃ¤chzen der KrÃ¤he.', 30, 7);
 
 --
 -- Indexes for dumped tables
